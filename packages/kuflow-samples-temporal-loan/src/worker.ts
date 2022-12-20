879e7cd86a01982675bc3f71b9f00856f0b38f60
Jorge Rodríguez Pedrianes <jrodped@kuflow.com>
2022-12-20 13:24:49 +0000

    [KF-192] Loan sample (#1)

---
+++ packages/kuflow-samples-temporal-loan/src/worker.ts
@@ -0,0 +1,163 @@
+/**
+ * The MIT License
+ * Copyright © 2021-present KuFlow S.L.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+import { KuFlowRestClient } from '@kuflow/kuflow-rest'
+import { createKuFlowAsyncActivities, createKuFlowSyncActivities } from '@kuflow/kuflow-temporal-activity-kuflow'
+import { KuFlowAuthorizationTokenProvider } from '@kuflow/kuflow-temporal-core'
+import { NativeConnection, Worker } from '@temporalio/worker'
+import fs from 'fs'
+import YAML from 'yaml'
+
+import * as activities from './activities'
+
+/**
+ * Run a Worker with an mTLS connection, configuration is provided via environment variables.
+ * Note that serverNameOverride and serverRootCACertificate are optional.
+ */
+async function main(): Promise<void> {
+  const workerProperties = loadConfiguration()
+
+  // Create a Temporal NativeConnection
+  const connection = await NativeConnection.connect({
+    address: workerProperties.temporal.target,
+    tls: {
+      serverNameOverride: workerProperties.temporal.mutualTls.serverNameOverride,
+      serverRootCACertificate: Buffer.from(workerProperties.temporal.mutualTls.caData),
+      // See docs for other TLS options
+      clientCertPair: {
+        crt: Buffer.from(workerProperties.temporal.mutualTls.certData),
+        key: Buffer.from(workerProperties.temporal.mutualTls.keyData),
+      },
+    },
+  })
+
+  // Instantiate KuFlow rest client
+  const kuFlowRestClient = new KuFlowRestClient(
+    {
+      clientId: workerProperties.kuflow.api.clientId,
+      clientSecret: workerProperties.kuflow.api.clientSecret,
+    },
+    {
+      endpoint: workerProperties.kuflow.api.endpoint,
+    },
+  )
+
+  // Create a KuFlowAuthorizationTokenProvider
+  const kuFlowTemporalConnection = KuFlowAuthorizationTokenProvider.instance({
+    temporalConnection: connection,
+    kuFlowRestClient,
+  })
+
+  const worker = await Worker.create({
+    connection,
+    namespace: workerProperties.temporal.namespace,
+    workflowsPath: require.resolve('./workflows'),
+    activities: {
+      ...createKuFlowSyncActivities(kuFlowRestClient),
+      ...createKuFlowAsyncActivities(kuFlowRestClient),
+      ...activities,
+    },
+    taskQueue: workerProperties.temporal.kuflowQueue,
+  })
+  console.log('Worker connection successfully established')
+
+  await worker.run()
+  await connection.close()
+  await kuFlowTemporalConnection.close()
+}
+
+main().catch(err => {
+  console.error(err)
+  process.exit(1)
+})
+
+// Helpers for configuring the mTLS client and worker samples
+
+export interface WorkerProperties {
+  kuflow: {
+    api: {
+      endpoint?: string
+      clientId: string
+      clientSecret: string
+    }
+  }
+  temporal: {
+    namespace: string
+    kuflowQueue: string
+    target: string
+    mutualTls: {
+      caData: string
+      certData: string
+      keyData: string
+      serverNameOverride?: string
+    }
+  }
+}
+
+export function loadConfiguration(): WorkerProperties {
+  const applicationFiles = fs.readFileSync('./application.yaml', 'utf8')
+
+  const applicationYaml = YAML.parse(applicationFiles)
+
+  return {
+    kuflow: {
+      api: {
+        endpoint: findProperty(applicationYaml, 'kuflow.api.endpoint'),
+        clientId: retrieveProperty(applicationYaml, 'kuflow.api.client-id'),
+        clientSecret: retrieveProperty(applicationYaml, 'kuflow.api.client-secret'),
+      },
+    },
+    temporal: {
+      target: retrieveProperty(applicationYaml, 'temporal.target'),
+      namespace: retrieveProperty(applicationYaml, 'temporal.namespace'),
+      kuflowQueue: retrieveProperty(applicationYaml, 'temporal.kuflow-queue'),
+      mutualTls: {
+        caData: retrieveProperty(applicationYaml, 'temporal.mutual-tls.ca-data'),
+        certData: retrieveProperty(applicationYaml, 'temporal.mutual-tls.cert-data'),
+        keyData: retrieveProperty(applicationYaml, 'temporal.mutual-tls.key-data'),
+        serverNameOverride: findProperty(applicationYaml, 'temporal.mutual-tls.server-name-override'),
+      },
+    },
+  }
+}
+
+function retrieveProperty(config: any, path: string): string {
+  const value = findProperty(config, path)
+  if (value == null) {
+    throw new ReferenceError(`${path} variable is not defined`)
+  }
+
+  return value
+}
+
+function findProperty(currentConfig: any, currentPath: string): string | undefined {
+  const [property, ...restPath] = currentPath.split('.')
+  const value = currentConfig[property]
+  if (value == null) {
+    return undefined
+  }
+  if (typeof value === 'object') {
+    return findProperty(value, restPath.join('.'))
+  }
+
+  return value?.toString()
+}
---
+++ packages/kuflow-samples-temporal-loan/src/workflows.ts
@@ -0,0 +1,187 @@
+/**
+ * The MIT License
+ * Copyright © 2021-present KuFlow S.L.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+import { getElementValueAsString, Process, Task } from '@kuflow/kuflow-rest'
+import type { createKuFlowAsyncActivities, createKuFlowSyncActivities } from '@kuflow/kuflow-temporal-activity-kuflow'
+import { WorkflowRequest, WorkflowResponse } from '@kuflow/kuflow-temporal-activity-kuflow'
+import { LoggerSinks, proxyActivities, proxySinks, uuid4 } from '@temporalio/workflow'
+
+import type * as activities from './activities'
+
+const kuFlowSyncActivities = proxyActivities<ReturnType<typeof createKuFlowSyncActivities>>({
+  startToCloseTimeout: '10 minutes',
+  scheduleToCloseTimeout: '356 days',
+})
+
+const kuFlowAsyncActivities = proxyActivities<ReturnType<typeof createKuFlowAsyncActivities>>({
+  startToCloseTimeout: '1 day',
+  scheduleToCloseTimeout: '356 days',
+})
+
+const { convert } = proxyActivities<typeof activities>({
+  startToCloseTimeout: '1 minute',
+})
+
+const { defaultWorkerLogger: logger } = proxySinks<LoggerSinks>()
+
+/** A workflow that simply calls an activity */
+export async function SampleEngineWorkerLoanWorkflow(workflowRequest: WorkflowRequest): Promise<WorkflowResponse> {
+  logger.info('Start', {})
+
+  const taskLoanApplication = await createTaskLoanApplication(workflowRequest)
+
+  const currency = getElementValueAsString(taskLoanApplication, 'currency')
+  const amount = getElementValueAsString(taskLoanApplication, 'amount')
+
+  const amountEUR = await convertToEuros(currency, amount)
+
+  let loanAuthorized = true
+  if (amountEUR > 5_000) {
+    const taskApproveLoan = await createTaskApproveLoan(taskLoanApplication, amountEUR)
+    const authorized = getElementValueAsString(taskApproveLoan, 'authorized')
+
+    loanAuthorized = authorized === 'OK'
+  }
+
+  const process = await retrieveProcess(workflowRequest)
+  if (loanAuthorized) {
+    await createTaskNotificationGranted(process)
+  } else {
+    await createTaskNotificationRejection(process)
+  }
+
+  await kuFlowSyncActivities.KuFlow_Engine_completeProcess({ processId: workflowRequest.processId })
+
+  logger.info('End', {})
+
+  return { message: 'OK' }
+}
+
+async function createTaskLoanApplication(workflowRequest: WorkflowRequest): Promise<Task> {
+  const taskId = uuid4()
+
+  await kuFlowAsyncActivities.KuFlow_Engine_createTaskAndWaitFinished({
+    task: {
+      objectType: 'TASK',
+      id: taskId,
+      processId: workflowRequest.processId,
+      taskDefinition: {
+        code: 'LOAN_APPLICATION',
+      },
+    },
+  })
+
+  const { task } = await kuFlowSyncActivities.KuFlow_Engine_retrieveTask({ taskId })
+
+  return task
+}
+
+/**
+ * Create a task in KuFlow to approve the loan due to doesn't meet the restrictions.
+ *
+ * @param taskLoanApplication task created to request a loan
+ * @param amountEUR amount requested
+ * @return task created
+ */
+async function createTaskApproveLoan(taskLoanApplication: Task, amountEUR: number): Promise<Task> {
+  const taskId = uuid4()
+
+  const firstName = getElementValueAsString(taskLoanApplication, 'firstName')
+  const lastName = getElementValueAsString(taskLoanApplication, 'lastName')
+
+  await kuFlowAsyncActivities.KuFlow_Engine_createTaskAndWaitFinished({
+    task: {
+      objectType: 'TASK',
+      id: taskId,
+      processId: taskLoanApplication.processId,
+      taskDefinition: {
+        code: 'APPROVE_LOAN',
+      },
+      elementValues: {
+        name: [{ type: 'STRING', value: `${firstName} ${lastName}` }],
+        amountRequested: [{ type: 'STRING', value: amountEUR.toString() }],
+      },
+    },
+  })
+
+  const { task } = await kuFlowSyncActivities.KuFlow_Engine_retrieveTask({ taskId })
+
+  return task
+}
+
+/**
+ * Create a notification task showing that the loan was granted.
+ *
+ * @param process Related process
+ */
+async function createTaskNotificationGranted(process: Process): Promise<void> {
+  const taskId = uuid4()
+
+  await kuFlowSyncActivities.KuFlow_Engine_createTask({
+    task: {
+      objectType: 'TASK',
+      id: taskId,
+      processId: process.id ?? '',
+      taskDefinition: {
+        code: 'NOTIFICATION_GRANTED',
+      },
+      owner: process.initiator,
+    },
+  })
+}
+
+/**
+ * Create a notification task showing that the loan was granted.
+ *
+ * @param process Related process
+ */
+async function createTaskNotificationRejection(process: Process): Promise<void> {
+  const taskId = uuid4()
+
+  await kuFlowSyncActivities.KuFlow_Engine_createTask({
+    task: {
+      objectType: 'TASK',
+      id: taskId,
+      processId: process.id ?? '',
+      taskDefinition: {
+        code: 'NOTIFICATION_REJECTION',
+      },
+      owner: process.initiator,
+    },
+  })
+}
+
+async function convertToEuros(currency: string, amount: string): Promise<number> {
+  if (currency === 'EUR') {
+    return parseFloat(amount)
+  }
+
+  const amountConverted = await convert(amount, currency, 'EUR')
+
+  return parseFloat(amountConverted)
+}
+
+async function retrieveProcess(workflowRequest: WorkflowRequest): Promise<Process> {
+  const { process } = await kuFlowSyncActivities.KuFlow_Engine_retrieveProcess({ processId: workflowRequest.processId })
+
+  return process
+}
